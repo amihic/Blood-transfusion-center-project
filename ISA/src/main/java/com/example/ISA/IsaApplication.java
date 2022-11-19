ceafe001df0955c66ec8e10f849d42af9895b24d
amihic <aleksa.mihic99@gmail.com>
2022-11-19 22:32:17 +0100

    dodata baza

--- ISA/src/main/java/com/example/ISA/IsaApplication.java
+++ ISA/src/main/java/com/example/ISA/IsaApplication.java
@@ -7,12 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
-
 import com.example.ISA.model.MedCenter;
 import com.example.ISA.model.Rating;
-import com.example.ISA.model.User;
 import com.example.ISA.repository.MedCenterRepository;
 import com.example.ISA.repository.RatingRepository;
 
@@ -33,12 +30,10 @@ public class IsaApplication implements CommandLineRunner{
 	@Override
 	public void run(String[] args) throws Exception
     {
-        // Inserting the data in the mysql table.
 		
-		
-		
-		        Rating newRating1 = new Rating(1,10,null);
-		        Rating newRating2 = new Rating(2,9,null);
+				
+		Rating newRating1 = new Rating(1,10,null);
+		Rating newRating2 = new Rating(2,9,null);
         Rating newRating3 = new Rating(3,9,null);
         Rating newRating4 = new Rating(4,8,null);
         Rating newRating5 = new Rating(5,7,null);
@@ -50,8 +45,8 @@ public class IsaApplication implements CommandLineRunner{
         
 
         
-		        ratingRepository.save(newRating1);
-		        ratingRepository.save(newRating2);
+	    ratingRepository.save(newRating1);
+		ratingRepository.save(newRating2);
         ratingRepository.save(newRating3);
         ratingRepository.save(newRating4);
         ratingRepository.save(newRating5);
@@ -61,21 +56,21 @@ public class IsaApplication implements CommandLineRunner{
         ratingRepository.save(newRating9);
         ratingRepository.save(newRating10);
         
-		        ArrayList<Rating> rate1 = new ArrayList<>();
-		        ArrayList<Rating> rate2 = new ArrayList<>();
-		        ArrayList<Rating> rate3 = new ArrayList<>();
-		        ArrayList<Rating> rate4 = new ArrayList<>();
-		        ArrayList<Rating> rate5 = new ArrayList<>();
-		        rate1.add(newRating1);
-		        rate1.add(newRating2);
-		        rate2.add(newRating3);
-		        rate2.add(newRating4);
-		        rate3.add(newRating5);
-		        rate3.add(newRating6);
-		        rate4.add(newRating7);
-		        rate4.add(newRating8);
-		        rate5.add(newRating9);
-		        rate5.add(newRating10);
+		ArrayList<Rating> rate1 = new ArrayList<>();
+		ArrayList<Rating> rate2 = new ArrayList<>();
+		ArrayList<Rating> rate3 = new ArrayList<>();
+		ArrayList<Rating> rate4 = new ArrayList<>();
+		ArrayList<Rating> rate5 = new ArrayList<>();
+		rate1.add(newRating1);
+		rate1.add(newRating2);
+		rate2.add(newRating3);
+		rate2.add(newRating4);
+		rate3.add(newRating5);
+		rate3.add(newRating6);
+		rate4.add(newRating7);
+		rate4.add(newRating8);
+		rate5.add(newRating9);
+		rate5.add(newRating10);
 		
 		
         MedCenter newMedCenter1 = new MedCenter(1, "MedCentarKV", "Jug Bogdanova 33", "Kraljevo", "Srbija", "Medicinski centar za davanje krvi", 0.0, rate1, null, null);
@@ -86,12 +81,7 @@ public class IsaApplication implements CommandLineRunner{
 
 
 
-        
-        
-        
-        
 
-        // ob.save() method
         medCenterRepository.save(newMedCenter1);
         medCenterRepository.save(newMedCenter2);
         medCenterRepository.save(newMedCenter3);
