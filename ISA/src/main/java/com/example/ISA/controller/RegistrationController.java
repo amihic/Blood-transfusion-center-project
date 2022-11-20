d96f2bb3c3a99d17c1232890110071bbc1eeba97
amihic <aleksa.mihic99@gmail.com>
2022-11-20 02:42:46 +0100

    Slanje email-a pri registraciji

--- ISA/src/main/java/com/example/ISA/controller/RegistrationController.java
+++ ISA/src/main/java/com/example/ISA/controller/RegistrationController.java
@@ -3,24 +3,37 @@ package com.example.ISA.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ISA.DTO.UserRegistrationDTO;
+import com.example.ISA.DTO.UserRegistrationDTO;
 import com.example.ISA.model.User;
 import com.example.ISA.repository.RegistrationRepository;
+import com.example.ISA.service.EmailService;
 import com.example.ISA.service.RegistrationService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @CrossOrigin("*")
 @RestController
 public class RegistrationController {
+	
+	private Logger logger = LoggerFactory.getLogger(UserController.class);
+
+	@Autowired
+	private EmailService emailService;
 	
 	@Autowired
 	private RegistrationService registrationService;
@@ -31,7 +44,18 @@ public class RegistrationController {
 	//sacuvaj usera
 	@RequestMapping(value="api/registration",method = RequestMethod.POST, consumes= MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> save(@RequestBody UserRegistrationDTO userDTO) throws Exception{
+		
+		
+		
 		User newUser = this.registrationService.save(userDTO);
+		try {
+			emailService.sendTestMail(newUser);
+		}
+		catch( Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
 		return new ResponseEntity<>(newUser, HttpStatus.CREATED);
 	}
 	
@@ -41,5 +65,25 @@ public class RegistrationController {
 		List<User> users = this.registrationRepository.findAll();
 		return new ResponseEntity<>(users, HttpStatus.OK);
 	}
+	
+	
+	@PostMapping("/signup/async")
+	public String signUpAsync(UserRegistrationDTO userRegistrationDTO){
+
+		//slanje emaila
+		try {
+			System.out.println("Thread id: " + Thread.currentThread().getId());
+			emailService.sendNotificaitionAsync(userRegistrationDTO);
+		}catch( Exception e ){
+			logger.info("Greska prilikom slanja emaila: " + e.getMessage());
+		}
+
+		return "success";
+	}
+	
+	
+	
+	
+	
 	
 }
