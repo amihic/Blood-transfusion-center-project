f085a6f8d359f448b96884c69993c912bd227eb3
amihic <aleksa.mihic99@gmail.com>
2023-06-08 21:30:12 +0200

    fun3.6 complaints on back

---
+++ ISA/src/main/java/com/example/ISA/controller/ComplaintController.java
@@ -0,0 +1,30 @@
+package com.example.ISA.controller;
+
+import com.example.ISA.model.Complaint;
+import com.example.ISA.service.ComplaintService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.ISA.repository.ComplaintRepository;
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/complaint")
+public class ComplaintController {
+    @Autowired
+    private ComplaintRepository complaintRepository;
+
+    @Autowired
+    private ComplaintService complaintService;
+
+    @RequestMapping(method = RequestMethod.POST, consumes= MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Complaint> createComplaint(@RequestBody Complaint complaint){
+        Complaint newComplaint = this.complaintService.save(complaint);
+        return new ResponseEntity<>(newComplaint, HttpStatus.CREATED);
+    }
+
+
+
+
+}
